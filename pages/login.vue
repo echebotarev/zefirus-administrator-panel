f5a5161032d4bb73588797d4074d6b952edd42d1
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-13 15:09:23 +0300

    Фавикон и текст

--- pages/login.vue
+++ pages/login.vue
@@ -97,13 +97,9 @@
     <!-- Auth fluid right content -->
     <div class="auth-fluid-right text-center">
       <div class="auth-user-testimonial">
-        <h2 class="mb-3">I love the color!</h2>
+        <h2 class="mb-3">Contactless. Timeless. Paperless.</h2>
         <p class="lead">
-          <i class="mdi mdi-format-quote-open"></i> It's a elegent templete. I
-          love it very much! . <i class="mdi mdi-format-quote-close"></i>
-        </p>
-        <p>
-          - Hyper Admin User
+          Hoteza Zefirus
         </p>
       </div>
       <!-- end auth-user-testimonial-->
