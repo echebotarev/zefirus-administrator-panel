e00d0b51d84338aa48b5844458217c9e1aed7c2b
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-14 17:02:13 +0300

    Список профайлов

--- pages/guest-profile/list.vue
+++ pages/guest-profile/list.vue
@@ -1,12 +1,69 @@
 <template>
-  <div>
-    <p>Hey! I'm Guest Profile List</p>
+  <div class="container-fluid">
+    <!-- start page title -->
+    <div class="row">
+      <div class="col-12">
+        <div class="page-title-box">
+          <h4 class="page-title">Guest Profile List</h4>
+        </div>
+      </div>
+    </div>
+    <!-- end page title -->
+
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table mb-0 table-striped tbody">
+                <thead>
+                  <tr>
+                    <th>Guest Name</th>
+                    <th>Birth date</th>
+                    <th>Country of birth</th>
+                    <th>Gender</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="profile in profiles" :key="profile.id" role="row">
+                    <td>
+                      {{ profile.person.lastName }}
+                      {{ profile.person.firstName }}
+                      (<nuxt-link :to="'/guest-profile/view/' + profile.id"
+                        >view</nuxt-link
+                      >)
+                    </td>
+                    <td>{{ profile.person.dateOfBirth }}</td>
+                    <td>{{ profile.person.countryOfBirth }}</td>
+                    <td>{{ profile.person.gender }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'List',
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('profiles/fetchProfiles')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable API server',
+      })
+    }
+  },
+  computed: mapState({
+    profiles: (state) => state.profiles.profiles,
+  }),
 }
 </script>
 
