41461799eaad93fadf26ee8e1035742407139cc5
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-14 13:57:44 +0300

    Active class + Logo href

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,71 +1,103 @@
 <template>
-  <div class="navbar-custom topnav-navbar topnav-navbar-dark">
-    <div class="container-fluid">
-      <!-- LOGO -->
-      <a href="" class="topnav-logo">
-        <span class="topnav-logo-lg">
-          <img
-            src="~/assets/images/logo_zefirus_white_h36.png"
-            alt=""
-            height="16"
-          />
-        </span>
-        <span class="topnav-logo-sm">
-          <img
-            src="~/assets/images/logo_zefirus_white_h36.png"
-            alt=""
-            height="16"
-          />
-        </span>
-      </a>
+  <div>
+    <div class="navbar-custom topnav-navbar topnav-navbar-dark">
+      <div class="container-fluid">
+        <!-- LOGO -->
+        <nuxt-link to="/" class="topnav-logo">
+          <span class="topnav-logo-lg">
+            <img
+              src="~/assets/images/logo_zefirus_white_h36.png"
+              alt=""
+              height="16"
+            />
+          </span>
+          <span class="topnav-logo-sm">
+            <img
+              src="~/assets/images/logo_zefirus_white_h36.png"
+              alt=""
+              height="16"
+            />
+          </span>
+        </nuxt-link>
 
-      <ul class="list-unstyled topbar-right-menu float-right mb-0">
-        <li class="dropdown notification-list">
-          <a
-            id="topbar-userdrop"
-            class="nav-link dropdown-toggle nav-user arrow-none mr-0"
-            data-toggle="dropdown"
-            href="#"
-            role="button"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <span class="account-user-avatar">
-              <img :src="avatarImg" alt="user-image" class="rounded-circle" />
-            </span>
-            <span>
-              <span class="account-user-name">Dominic Keller</span>
-              <span class="account-position">Founder</span>
-            </span>
-          </a>
-          <div
-            class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown"
-            aria-labelledby="topbar-userdrop"
-          >
-            <!-- item-->
-            <a href="javascript:void(0);" class="dropdown-item notify-item">
-              <i class="mdi mdi-account-circle mr-1"></i>
-              <span>My Account</span>
-            </a>
-
-            <!-- item-->
+        <ul class="list-unstyled topbar-right-menu float-right mb-0">
+          <li class="dropdown notification-list">
             <a
-              href="http://support.hoteza.com"
-              target="_blank"
-              class="dropdown-item notify-item"
+              id="topbar-userdrop"
+              class="nav-link dropdown-toggle nav-user arrow-none mr-0"
+              data-toggle="dropdown"
+              href="#"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
             >
-              <i class="mdi mdi-lifebuoy mr-1"></i>
-              <span>Support</span>
+              <span class="account-user-avatar">
+                <img :src="avatarImg" alt="user-image" class="rounded-circle" />
+              </span>
+              <span>
+                <span class="account-user-name">Dominic Keller</span>
+                <span class="account-position">Founder</span>
+              </span>
             </a>
+            <div
+              class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown"
+              aria-labelledby="topbar-userdrop"
+            >
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <i class="mdi mdi-account-circle mr-1"></i>
+                <span>My Account</span>
+              </a>
 
-            <!-- item-->
-            <a href="javascript:void(0);" class="dropdown-item notify-item">
-              <i class="mdi mdi-logout mr-1"></i>
-              <span>Logout</span>
-            </a>
+              <!-- item-->
+              <a
+                href="http://support.hoteza.com"
+                target="_blank"
+                class="dropdown-item notify-item"
+              >
+                <i class="mdi mdi-lifebuoy mr-1"></i>
+                <span>Support</span>
+              </a>
+
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <i class="mdi mdi-logout mr-1"></i>
+                <span>Logout</span>
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="topnav shadow-sm">
+      <div class="container-fluid">
+        <nav class="navbar navbar-light navbar-expand-lg topnav-menu">
+          <div class="collapse navbar-collapse" id="topnav-menu-content">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <nuxt-link
+                  to="/reservation/list"
+                  class="nav-link"
+                  role="button"
+                >
+                  <i class="mdi mdi-notebook-outline mr-1"></i>
+                  Reservation List
+                </nuxt-link>
+              </li>
+              <li class="nav-item">
+                <nuxt-link
+                  to="/guest-profile/list"
+                  class="nav-link"
+                  role="button"
+                >
+                  <i class="mdi mdi-account-multiple-outline mr-1"></i>
+                  Guest Profiles List
+                </nuxt-link>
+              </li>
+            </ul>
           </div>
-        </li>
-      </ul>
+        </nav>
+      </div>
     </div>
   </div>
 </template>
@@ -77,7 +109,7 @@ export default {
   name: 'NavBar',
   computed: mapState({
     user: (state) => state.user.user,
-    avatarImg: (state) => `images/users/${state.user.user.image}`,
+    avatarImg: (state) => `/images/users/${state.user.user.image}`,
   }),
   created() {
     this.fetchUser()
--- nuxt.config.js
+++ nuxt.config.js
@@ -70,4 +70,8 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
+
+  router: {
+    linkActiveClass: 'active',
+  },
 }
