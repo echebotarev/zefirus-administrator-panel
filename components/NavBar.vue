9dcac3475d43dc1f558e321bca30b9bdd9249e07
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-14 13:27:52 +0300

    Удалил лишние пункты меню

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -43,32 +43,19 @@
             aria-labelledby="topbar-userdrop"
           >
             <!-- item-->
-            <div class="dropdown-header noti-title">
-              <h6 class="text-overflow m-0">Welcome !</h6>
-            </div>
-
-            <!-- item-->
             <a href="javascript:void(0);" class="dropdown-item notify-item">
               <i class="mdi mdi-account-circle mr-1"></i>
               <span>My Account</span>
             </a>
 
             <!-- item-->
-            <a href="javascript:void(0);" class="dropdown-item notify-item">
-              <i class="mdi mdi-account-edit mr-1"></i>
-              <span>Settings</span>
-            </a>
-
-            <!-- item-->
-            <a href="javascript:void(0);" class="dropdown-item notify-item">
+            <a
+              href="http://support.hoteza.com"
+              target="_blank"
+              class="dropdown-item notify-item"
+            >
               <i class="mdi mdi-lifebuoy mr-1"></i>
               <span>Support</span>
-            </a>
-
-            <!-- item-->
-            <a href="javascript:void(0);" class="dropdown-item notify-item">
-              <i class="mdi mdi-lock-outline mr-1"></i>
-              <span>Lock Screen</span>
             </a>
 
             <!-- item-->
